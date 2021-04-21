57af20150544a1155f0f5df7661eb4f333204220
dudes <dudes@DESKTOP-2PD2A26>
2021-04-21 13:31:48 -0500

    1.0

---
+++ src/main/java/backend/Activity.java
@@ -0,0 +1,122 @@
+/*
+ * 
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+import java.util.*;
+import java.time.*;
+import java.io.Serializable;
+import static java.time.temporal.ChronoUnit.MINUTES;
+/**
+ *
+ * @author Benjamin Jones
+ */
+public class Activity implements Serializable{
+    protected LocalTime startTime;
+    protected LocalTime stopTime;
+    protected LocalDate date;
+    protected String location;
+    protected String extraInfo;
+    protected long duration;
+    
+    public Activity()
+    {
+    	startTime = null;
+        stopTime = null;
+    	date = null;
+    	location = "";
+    	extraInfo = "";
+        duration = 60;
+    }
+    
+    public Activity(LocalTime startTime_, LocalDate date_, String location_, String extraInfo_)
+    {
+    	startTime = startTime_;
+    	date = date_;
+    	location = location_;
+    	extraInfo = extraInfo_;
+    }
+    
+    public Activity(LocalTime startTime_, LocalTime stopTime_, LocalDate date_, String location_, String extraInfo_)
+    {
+    	startTime = startTime_;
+        stopTime = stopTime_;
+    	date = date_;
+    	location = location_;
+    	extraInfo = extraInfo_;
+        duration = startTime.until(stopTime, MINUTES);
+    }
+    
+    public Activity(LocalTime startTime_, LocalDate date_, String location_, String extraInfo_, long duration_)
+    {
+    	startTime = startTime_;
+    	date = date_;
+    	location = location_;
+    	extraInfo = extraInfo_;
+        duration = duration_;
+        stopTime = startTime.plusMinutes(duration);
+    }
+    
+    public void setstopTime(LocalTime newTime)
+    {
+        stopTime = newTime;
+    }
+    
+    public void setstartTime(LocalTime newTime)
+    {
+        startTime = newTime;
+    }
+    
+    public void setDate(LocalDate newDate)
+    {
+        date = newDate;
+    }
+    
+    public void setLocation(String newLocation)
+    {
+        location = newLocation;
+    }
+    
+    public void setExtraInfo(String newExtraInfo)
+    {
+        extraInfo = newExtraInfo;
+    }
+    
+    public void setDuration(long duration_)
+    {
+        duration = duration_;
+    }
+    
+    public LocalTime getStartTime()
+    {
+        return startTime;
+    }
+    
+    public LocalTime getStopTime()
+    {
+        return startTime;
+    }
+    
+    public LocalDate getDate()
+    {
+        return date;
+    }
+    
+    public String getLocation()
+    {
+        return location;
+    }
+    
+    public String getExtraInfo()
+    {
+        return extraInfo;
+    }
+    
+    public long getDuration()
+    {
+        return duration;
+    }
+    
+}
+
