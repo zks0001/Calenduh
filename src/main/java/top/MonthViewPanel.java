83b070d7c16b25355e9cb6926830d3244482a371
dudes <dudes@DESKTOP-2PD2A26>
2021-04-23 11:53:44 -0500

    1.3, Added setText() method in DayPanel, and example in monthviewPanel

--- src/main/java/top/MonthViewPanel.java
+++ src/main/java/top/MonthViewPanel.java
@@ -82,6 +82,7 @@ public class MonthViewPanel extends javax.swing.JPanel {
     
     private ArrayList<DayPanel> days;
     private ArrayList<JLabel> weekday;
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
