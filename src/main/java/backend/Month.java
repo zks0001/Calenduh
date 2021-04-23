b2810d5b2776fee8914af4b8e2b980d1a7aa758f
dudes <dudes@DESKTOP-2PD2A26>
2021-04-23 12:51:22 -0500

    1.4, Day and Month now exist

---
+++ src/main/java/backend/Month.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import java.time.YearMonth;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dudes
+ */
+public class Month {
+    protected YearMonth when;           
+    protected Integer length;
+    protected Integer offset;
+    public ArrayList Days = new ArrayList<Activity>();; 
+            
+            
+    public Month(YearMonth when_)
+    {
+        when = when_;
+        length = when.lengthOfMonth();
+        offset = (when.atDay(1).getDayOfWeek().getValue()) % 7;
+        Days = new ArrayList<>(length);
+    }
+    
+    public void append(Day obj)
+    {
+        Days.add(obj);
+    }
+    
+    public void rem(Day obj)
+    {
+        Days.remove(obj);
+    }
+}
