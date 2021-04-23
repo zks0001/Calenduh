5cb7c1cc02d43356a558dd775a6d7652c548d539
andrewhthornton <82624782+andrewhthornton@users.noreply.github.com>
2021-04-23 12:42:15 -0500

    Add files via upload

---
+++ src/main/java/backend/Day.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cs.project.main;
+
+import java.util.*;
+import java.time.*;
+/**
+ *
+ * @author jjdre
+ */
+public class Day 
+{
+    protected LocalDate date;
+    protected int arrSize = 0;
+    public ArrayList activityList = new ArrayList<Activity>();
+    
+    public Day(LocalDate date_)
+    {
+        date = date_;
+    }
+    
+    public void Add(Activity a)
+    {
+        activityList.add(a);
+        arrSize = arrSize + 1;
+    }
+    
+    public void Remove(Activity a)
+    {
+        activityList.remove(a);
+        arrSize = arrSize - 1;
+    }
+    
+}
